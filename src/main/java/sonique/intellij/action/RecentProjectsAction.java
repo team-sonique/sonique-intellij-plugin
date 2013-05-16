c61bed12b35ebfd4d3478f280b91977abde96e34
Sonique <team.sonique.sns@bskyb.com>
2013-05-16 09:07:10 +0000

    Paul : Adding project location to project switcher popup.
    git-svn-id: http://subversion.sns.sky.com/svn/netstream/trunk/sonique-workshop@207523 5ac20129-5319-0410-b4e6-93586071aa28

--- src/main/java/sonique/intellij/action/RecentProjectsAction.java
+++ src/main/java/sonique/intellij/action/RecentProjectsAction.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.project.ProjectManagerAdapter;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.PopupChooserBuilder;
 import com.intellij.openapi.util.IconLoader;
@@ -15,38 +16,50 @@ import com.intellij.ui.components.JBList;
 import javax.swing.*;
 import java.awt.*;
 
-public class RecentProjectsAction extends AnAction implements DumbAware {
-    public void actionPerformed(AnActionEvent e) {
-        Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
+import static com.intellij.ui.SimpleTextAttributes.REGULAR_ATTRIBUTES;
+import static com.intellij.ui.SimpleTextAttributes.REGULAR_ITALIC_ATTRIBUTES;
 
-        DefaultListModel model = new DefaultListModel();
-        for (Project openProject : openProjects) {
-            model.addElement(openProject);
+public class RecentProjectsAction extends AnAction implements DumbAware {
+
+    private final DefaultListModel listModel;
+    private final ProjectManager projectManager;
+
+    public RecentProjectsAction() {
+        projectManager = ProjectManager.getInstance();
+        listModel = new DefaultListModel();
+        initialiseProjectModel(projectManager);
+        projectManager.addProjectManagerListener(new ProjectListener());
+    }
+
+    private void initialiseProjectModel(ProjectManager projectManager) {
+        for (Project openProject : projectManager.getOpenProjects()) {
+            listModel.addElement(openProject);
+        }
+    }
+
+    public void actionPerformed(AnActionEvent e) {
+        Project currentProject = e.getProject();
+        listModel.removeElement(currentProject);
+        listModel.add(0, currentProject);
+        JList projectList = new JBList(listModel);
+
+        if (listModel.getSize() > 1) {
+            projectList.setSelectedIndex(1);
+        } else {
+            projectList.setSelectedIndex(0);
         }
 
-        JList list = new JBList(model);
-        list.setCellRenderer(new RecentProjectsRenderer());
-
-        JBPopup popup = new PopupChooserBuilder(list)
+        projectList.setCellRenderer(new RecentProjectsRenderer());
+        JBPopup popup = new PopupChooserBuilder(projectList)
                 .setTitle("Recent Projects")
-                .setItemChoosenCallback(new SelectProjectRunnable(list))
+                .setItemChoosenCallback(new SelectProjectRunnable(projectList))
                 .createPopup();
 
-        popup.showCenteredInCurrentWindow(e.getProject());
+        popup.showCenteredInCurrentWindow(currentProject);
     }
 
-    private static class RecentProjectsRenderer extends ColoredListCellRenderer {
-        @Override
-        protected void customizeCellRenderer(JList jList, Object value, int i, boolean b, boolean b2) {
-            if(value instanceof Project) {
-                setIcon(IconLoader.findIcon("/nodes/ideaProject.png"));
-                Project project = (Project) value;
-                append(project.getName());
-            }
-        }
-    }
 
-    private static class SelectProjectRunnable implements Runnable {
+    private class SelectProjectRunnable implements Runnable {
         private final JList list;
 
         public SelectProjectRunnable(JList list) {
@@ -56,15 +69,45 @@ public class RecentProjectsAction extends AnAction implements DumbAware {
         @Override
         public void run() {
             Project selectedProject = (Project) list.getSelectedValue();
+            listModel.removeElement(selectedProject);
+            listModel.add(0, selectedProject);
+
             JFrame projectFrame = WindowManager.getInstance().getFrame(selectedProject);
 
             int frameState = projectFrame.getExtendedState();
             if ((frameState & Frame.ICONIFIED) == Frame.ICONIFIED) {
-              // restore the frame if it is minimized
-              projectFrame.setExtendedState(frameState ^ Frame.ICONIFIED);
+                // restore the frame if it is minimized
+                projectFrame.setExtendedState(frameState ^ Frame.ICONIFIED);
             }
             projectFrame.toFront();
             projectFrame.requestFocus();
+        }
+    }
+
+    private class ProjectListener extends ProjectManagerAdapter {
+
+        @Override
+        public void projectOpened(Project project) {
+            listModel.addElement(project);
+        }
+
+        @Override
+        public void projectClosed(Project project) {
+            listModel.removeElement(project);
+        }
+    }
+
+    private class RecentProjectsRenderer extends ColoredListCellRenderer {
+        @Override
+        protected void customizeCellRenderer(JList jList, Object value, int index, boolean isSelected, boolean hasFocus) {
+            if (value instanceof Project) {
+                setIcon(IconLoader.findIcon("/nodes/ideaProject.png"));
+                Project project = (Project) value;
+
+                append(project.getName(), REGULAR_ATTRIBUTES, true);
+                append("   ");
+                append("[" + project.getPresentableUrl() + "]", REGULAR_ITALIC_ATTRIBUTES);
+            }
         }
     }
 }
